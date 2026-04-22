7277a3f46503e7d36b7f57d886399b86dad3e255
rodrigor23r-cmyk <rodrigor23r@gmail.com>
2026-04-22 13:35:47 +0200

    a la antigua, sin record pattern

--- record-pattern/src/main/java/com/example/App.java
+++ record-pattern/src/main/java/com/example/App.java
@@ -1,10 +1,39 @@
 package com.example;
 
-/**
- * Hello world!
- */
+
+
 public class App {
+	
+	/**
+	 * @version 12
+	 * @author Rodrigo R F;
+	 * 
+	 * 
+	 * Método que recibe objeto y muestra detalles del mismo
+	 * con if, Pattern Matching, instanceof de un record
+	 */
+	static void imprimeDetalles(Object obj) {
+		
+		// antes de Record Pattern:
+		if (obj instanceof Libro libro) {
+			System.out.println("Título: " + libro.titulo());
+			System.out.println("Autor: " + libro.autor());
+		}
+	}
+	
+	
+	
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        
+    	// Crear objeto de tipo Record
+    	// Libro objlibro = new Libro("Java, un café muy rico", "Ramiro");
+    	// creo otra ver objeto pero con builder
+    	Libro objLibro = Libro.builder()
+    			.titulo("Java, ese café exótico")
+    			.autor("Ramiro")
+    			.build();
+    	
+    	imprimeDetalles(objLibro);
+    	
     }
 }
---
+++ record-pattern/src/main/java/com/example/Libro.java
@@ -0,0 +1,9 @@
+package com.example;
+
+import lombok.Builder;
+
+@Builder
+
+public record Libro(String titulo, String autor) {
+
+}
